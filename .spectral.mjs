e29efe3a48bb1bcab0654be9fb343bbab411c8ed
haluk@pembepanjur.com <haluk@pembepanjur.com>
2023-04-01 15:35:58 +0300

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5MzUwNg/spectral.js?branch_slug=master&token=b1f5a1b6-bf43-4c59-977d-670ad2dcdcac";
+export default { extends: ruleset };
